019d9b9db493f3448ca09f60982bde720e99ed8f
Rafael-Puerta <rafapc1992@gmail.com>
2022-09-30 18:03:35 +0200

    Mejorado

--- app/src/main/java/com/example/endevina/MainActivity.java
+++ app/src/main/java/com/example/endevina/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.endevina;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    static int numero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -20,28 +21,38 @@ public class MainActivity extends AppCompatActivity {
         final Button button = findViewById(R.id.button);
         final TextView historial = findViewById(R.id.histo);
         final TextView intentos = findViewById(R.id.intent);
-        int numero=(int) (Math.random()*100);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        MainActivity.numero=(int) (Math.random()*100);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-
-
                 if(Integer.parseInt(texto.getText().toString())>numero){
-                    historial.append("EL NUMERO ES MAS PEQUEÑO!\n");
-
-
+                    historial.append("Introduciste: " + texto.getText().toString() + " EL NUMERO ES MAS PEQUEÑO!\n");
+                    intentos.setText(String.valueOf(Integer.parseInt(intentos.getText().toString())+1));
+                    texto.setText("");
                 }
                 else if(Integer.parseInt(texto.getText().toString())<numero){
-                    historial.append("EL NUMERO ES MAS GRANDE!\n");
-
-
+                    historial.append("Introduciste: " + texto.getText().toString() + " EL NUMERO ES MAS GRANDE!\n");
+                    intentos.setText(String.valueOf(Integer.parseInt(intentos.getText().toString())+1));
+                    texto.setText("");
                 }
                 else{
                     historial.append("EL NUMERO ES CORRECTOOOOOOOOOOOO!\n");
-
+                    builder.setTitle("GANADOR");
+                    builder.setMessage("HAS ACERTADO EL NUMERO");
+                    builder.setPositiveButton("OK",null);
+                    builder.create();
+                    builder.show();
+                    texto.setText("");
+                    intentos.setText("0");
+                    historial.setText("Historial: ");
+                    MainActivity.numero=(int) (Math.random()*100);
                 }
-                intentos.setText(String.valueOf(Integer.parseInt(intentos.getText().toString())+1));
+
+
             }
+
         });
     }
 }
---
+++ app/src/main/java/com/example/endevina/Ranking.java
@@ -0,0 +1,20 @@
+package com.example.endevina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Ranking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+        Intent intent = getIntent();
+
+    }
+
+}
